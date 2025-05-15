6279052a011f396b3d4c50bd3ebe9aaff9415fce
w-xuefeng <w-xuefeng@outlook.com>
2025-05-15 17:12:43 +0800

    fix: fixed preview for playground

--- packages/domily-playground/src/view/layout/index.ts
+++ packages/domily-playground/src/view/layout/index.ts
@@ -4,92 +4,95 @@ import Editor from "../editor";
 import Preview from "../preview";
 
 export default function Layout() {
-  const initialCode = `<style>body{ margin: 0; }</style>
+  const initialCode = `<style>
+  body {
+    margin: 0;
+  }
+</style>
 
 <div id="app"></div>
 
 <script type="module">
-import { signal, render } from 'https://cdn.jsdelivr.net/npm/@domily/runtime-core/lib/index.esm.js';
+  import { signal, render } from 'https://cdn.jsdelivr.net/npm/@domily/runtime-core/lib/index.esm.js';
 
-function App() {
-  const count = signal(0);
-  const css = {
-    ".domily-example": {
-      width: "100%",
-      height: "100%",
-      display: "flex",
-      gap: "10px",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#191919",
-      color: "#ffffff",
-      ".logo": {
-        height: "100px",
-        animationName: "scale",
-        animationDuration: "2s",
-        animationPlayState: "running",
-        animationIterationCount: "infinite",
-        animationTimingFunction: "linear",
-        "&:hover": {
-          animationPlayState: "paused",
+  function App() {
+    const count = signal(0);
+    const css = {
+      ".domily-example": {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        gap: "10px",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#191919",
+        color: "#ffffff",
+        ".logo": {
+          height: "100px",
+          animationName: "scale",
+          animationDuration: "2s",
+          animationPlayState: "running",
+          animationIterationCount: "infinite",
+          animationTimingFunction: "linear",
+          "&:hover": {
+            animationPlayState: "paused",
+          },
+        },
+        button: {
+          boxSizing: "border-box",
+          cursor: "pointer",
+          color: "#aac8e4",
+          backgroundColor: "#2f2f2f",
+          background: "linear-gradient(#2f2f2f, #2f2f2f) padding-box, linear-gradient(45deg, #fff500, #00e1ff) border-box",
+          border: "2px solid transparent",
+          fontSize: "16px",
+          padding: "8px 18px",
+          fontWeight: 500,
+          borderRadius: "8px",
         },
       },
-      button: {
-        boxSizing: "border-box",
-        cursor: "pointer",
-        color: "#aac8e4",
-        backgroundColor: "#2f2f2f",
-        background: "linear-gradient(#2f2f2f, #2f2f2f) padding-box, linear-gradient(45deg, #fff500, #00e1ff) border-box",
-        border: "2px solid transparent",
-        fontSize: "16px",
-        padding: "8px 18px",
-        fontWeight: 500,
-        borderRadius: "8px",
-      },
-    },
-    "@keyframes scale": {
-      "100%": {
-        transform: "scale(1.2)"
-      },
-    }
-  };
-  return {
-    tag: "fragment",
-    customElement: {
-      enable: true,
-      name: "domily-counter-example",
-      useShadowDOM: true,
-      shadowDOMMode: "open",
-      css,
-    },
-    className: "domily-example",
-    children: [
-      {
-        tag: "img",
-        className: "logo",
-        props: {
-          src: "https://raw.githubusercontent.com/w-xuefeng/domily/main/assets/logo.webp"
-        }
-      },
-      {
-        tag: "button",
-        text: "Increment",
-        on: {
-          click: () => count(count() + 1),
+      "@keyframes scale": {
+        "100%": {
+          transform: "scale(1.2)"
         },
+      }
+    };
+    return {
+      tag: "fragment",
+      customElement: {
+        enable: true,
+        name: "domily-counter-example",
+        useShadowDOM: true,
+        shadowDOMMode: "open",
+        css,
       },
-      {
-        tag: "p",
-        text: () => "Count:" + count(),
-      },
-    ],
-  };
-}
+      className: "domily-example",
+      children: [
+        {
+          tag: "img",
+          className: "logo",
+          props: {
+            src: "https://raw.githubusercontent.com/w-xuefeng/domily/main/assets/logo.webp"
+          }
+        },
+        {
+          tag: "button",
+          text: "Increment",
+          on: {
+            click: () => count(count() + 1),
+          },
+        },
+        {
+          tag: "p",
+          text: () => "Count:" + count(),
+        },
+      ],
+    };
+  }
 
-render(App()).mount("#app");
-</script>
-  `;
+  render(App()).mount("#app");
+</script>`;
 
   const code = signal(initialCode);
 
